146bf36c0293e248909d9e3751d5d743e7d846cd
Taylor J Saintable <taylor.j.saintable@gmail.com>
2024-08-31 15:21:32 -0400

    UI Fixes

--- FundWatch/obj/Debug/net8.0/FundWatch.AssemblyInfo.cs
+++ FundWatch/obj/Debug/net8.0/FundWatch.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FundWatch")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+78dbe9a7dcdb6213090c48347907e217dbf0f42e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ff8561899efb380029e14013d50433a27b41c3e3")]
 [assembly: System.Reflection.AssemblyProductAttribute("FundWatch")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FundWatch")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
