61cacae2e331e09f311674b31d66ac6ea89f6742
Taylor J Saintable <taylor.j.saintable@gmail.com>
2025-04-30 12:48:41 -0400

    Add data protection

--- FundWatch/Migrations/ApplicationDbContextModelSnapshot.cs
+++ FundWatch/Migrations/ApplicationDbContextModelSnapshot.cs
@@ -157,6 +157,25 @@ namespace FundWatch.Migrations
                     b.ToTable("AppWatchlists", (string)null);
                 });
 
+            modelBuilder.Entity("Microsoft.AspNetCore.DataProtection.EntityFrameworkCore.DataProtectionKey", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd()
+                        .HasColumnType("int");
+
+                    SqlServerPropertyBuilderExtensions.UseIdentityColumn(b.Property<int>("Id"));
+
+                    b.Property<string>("FriendlyName")
+                        .HasColumnType("nvarchar(max)");
+
+                    b.Property<string>("Xml")
+                        .HasColumnType("nvarchar(max)");
+
+                    b.HasKey("Id");
+
+                    b.ToTable("DataProtectionKeys");
+                });
+
             modelBuilder.Entity("Microsoft.AspNetCore.Identity.IdentityRole", b =>
                 {
                     b.Property<string>("Id")
